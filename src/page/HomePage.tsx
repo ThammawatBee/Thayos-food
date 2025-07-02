9e59fbe600af6e61cab02950996a5ba9b3c590df
Bee <thammawat@muze.co.th>
2025-07-02 08:25:00 +0700

    fix homepage menu path

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -19,7 +19,7 @@ const HomePage = () => {
       <Text textStyle="3xl" fontWeight="bold">Select Menu</Text>
       <Box display='flex' alignItems='center' justifyContent='center' marginTop={'30px'}>
         <Box marginRight={"50px"}>
-          <Link to="/equipment">
+          <Link to="/">
             <Box display="flex" alignItems='center' justifyContent='center' borderColor="black" borderWidth="1px" width={'200px'} height={'200px'} background={'white'} borderRadius="20px"
             // _hover={{ background: '#F4F4F5' }}
             >
@@ -29,7 +29,7 @@ const HomePage = () => {
           <Text textAlign='center' textStyle="lg" marginTop={'15px'}>Monitoring Status</Text>
         </Box>
         <Box marginRight={"50px"}>
-          <Link to="/inspection">
+          <Link to="/order">
             <Box display="flex" alignItems='center' justifyContent='center' borderColor="black" borderWidth="1px" width={'200px'} height={'200px'} background={'white'} borderRadius="20px"
             // _hover={{ background: '#F4F4F5' }}
             >
@@ -39,7 +39,7 @@ const HomePage = () => {
           <Text textAlign='center' textStyle="lg" marginTop={'15px'}>Update Order</Text>
         </Box>
         <Box>
-          <Link to="/report">
+          <Link to="/customer-management">
             <Box display="flex" alignItems='center' justifyContent='center' borderColor="black" borderWidth="1px" width={'200px'} height={'200px'} background={'white'} borderRadius="20px"
             // _hover={{ background: '#F4F4F5' }}
             >
@@ -51,7 +51,7 @@ const HomePage = () => {
       </Box>
       <Box display='flex' alignItems='center' justifyContent='center' marginTop={'30px'}>
         <Box marginRight={"50px"}>
-          <Link to="/equipment">
+          <Link to="/calendar">
             <Box display="flex" alignItems='center' justifyContent='center' borderColor="black" borderWidth="1px" width={'200px'} height={'200px'} background={'white'} borderRadius="20px"
             // _hover={{ background: '#F4F4F5' }}
             >
@@ -71,7 +71,7 @@ const HomePage = () => {
           <Text textAlign='center' textStyle="lg" marginTop={'15px'}>User Management</Text>
         </Box> : null}
         <Box>
-          <Link to="/report">
+          <Link to="/">
             <Box display="flex" alignItems='center' justifyContent='center' borderColor="black" borderWidth="1px" width={'200px'} height={'200px'} background={'white'} borderRadius="20px"
             // _hover={{ background: '#F4F4F5' }}
             >
